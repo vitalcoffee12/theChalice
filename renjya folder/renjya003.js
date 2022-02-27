52763da012c10730b1f9a96d6804847ec9abb75e
DESKTOP-VEUDO2O\SAMSUNG <anznfh5@gmail.com>
2022-02-27 22:54:18 +0900

    렌쟈 스터디 3번째 파일

---
+++ renjya folder/renjya003.js
@@ -0,0 +1,50 @@
+
+// == 는 같다, <= 작거나 같다, !=같지않다, > 크다, >= 크거나 같다
+// && 그리고, || 또는, ! 아니다
+
+/* 여러줄 주석
+입니다!!
+*/
+
+
+console.log(3<=4);
+console.log(3==3);
+
+console.log(2!=2);
+console.log(!(3!=4));
+console.log(!(!(3!=4)));
+console.log(3==3 && 4==6);
+console.log(3==3 || 3==6);
+
+
+if(false){console.log("정답입니다~");}
+else if(true){console.log("else if");}
+else if(true){console.log("else if 2");}
+else {console.log("else");}
+/*
+var math=70;
+
+if(math==100){
+    console.log("A등급입니다.");
+}
+else if(math<100 && math>=80){
+    console.log("B등급입니다.");
+}
+else if(math<80 && math>=60){
+    console.log("C등급입니다.");
+}
+else {
+    console.log("재수강하십쇼.");
+}
+*/
+
+/*
+조금만 간단히 말하자면 조건이1개다 = if문
+조건이 2개 . 1개의 조건과 조건 이외의 값 을 걸러내고싶다 = if /else 문
+조건이 3개이상이다 if /else if / else 문
+*/
+
+var 렌쟈 = "16길"
+
+if(렌쟈=="16길" || 렌쟈=="백마도사"){console.log("참담한 마음")}
+
