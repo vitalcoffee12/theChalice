5a98b7165e3787e719d3f275dcfba131ffd454f1
DESKTOP-O8ARQMO\송준우 <junwoosara@naver.com>
2022-02-27 22:54:23 +0900

    2022/02/27 조건문 if - rillasong

--- rillasong/practice/javascript/prac.js
+++ rillasong/practice/javascript/prac.js
@@ -1,1 +1,15 @@
-console.log('123가나다라');
+var math=50;
+
+if(math==100){
+    console.log("A등급입니다.");
+}
+else if(math<100 && math>=80){
+    console.log("B등급입니다.");
+}
+else if(math<80 && math>=60){
+    console.log("C등급입니다.");
+}
+else {
+    console.log("재수강하십쇼.");
+}
+
