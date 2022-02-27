8d041f37dd1a52f4d55f3fa3c4d716d838330764
DESKTOP-KJH\User <zscf1212@naver.com>
2022-02-27 22:54:46 +0900

    no message

---
+++ KJH/220227.js
@@ -0,0 +1,43 @@
+
+console.log(3 >= 4);
+console.log(5 != 4);
+
+console.log(!(3 != 4))
+console.log((3 == 3 && 4 == 4))
+console.log(!(3 == 3 || 4 == 4))
+
+/** 
+ * 
+ * */ 
+if(3 == 3){console.log("정답입니다")}
+
+if (false) {
+    console.log("정답입니다");
+  } else if (true) {
+    console.log("else if");
+  } else if (true) {
+    console.log("else if 2");
+  } else {
+    console.log("else");
+  }
+
+
+  var math = 100;
+  if(math==100){
+    console.log("A등급입니다.");
+}
+else if(80<=math && math<100){
+    console.log("B등급입니다.");
+}
+else if(60<=math && math<80){
+    console.log("C등급입니다.");
+}
+else {
+    console.log("재수강하십쇼.");
+}
+
+var 흑당 = "암흑기사";
+var 흑당 = "건브레이커";
+var 흑당 = "전사";
+
+if(흑당){console.log(흑당)}
