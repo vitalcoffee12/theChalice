de5f86cd957c5d7996af02abff952c137c62685e
vc12 <vitalcoffee12@gmail.com>
2022-02-27 23:00:34 +0900

    javascript 03-3

---
+++ example/javascript/js_chapter03-3.js
@@ -0,0 +1,134 @@
+/** 조건문
+ *
+ *
+ */
+if (3 == 4) {
+  console.log("정답입니다");
+}
+
+// 한줄짜리 주석
+
+/*
+여러줄 짜리
+주석입니다
+*/
+
+/**
+ * 왠지 모르겠어 별나오는 주석
+ * 조건문
+
+조건식 
+참, 거짓 (true, false)
+boolean (불린, 불리언) 
+논리 자료형
+
+연산자
+'<, <=' 작거나 같다 ('≤'0 
+'>, >=' 크거나 같다
+'=='    같다 할당연산자X
+'!='    다르다 
+
+console.log(3 <= 4);
+true, (옳다)
+
+
+폴더 이동 cd (폴더이름)
+cd example/javscript
+ 
+node javscript (파일이름)
+
+console.log(5 != 3);
+> true;
+논리연산자 
+<and or not>
+&& and (그리고) 
+둘다 참일 경우에만 결과가 참
+  
+|| or  (또는)
+하나만 참이어도 결과가 참
+ 
+!조건식 => 반대로  not (아니다)
+참 -=> 거짓
+거짓 => 참
+console.log(!(3 != 4));
+> false
+
+console.log(true || false);
+> true
+난 몰라 마인크래프트
+
+
+if -else
+
+switch 
+if(조건식) {
+console.log('');
+}
+
+조건식이 참일 경우에만
+
+if(false) {
+console.log("정답입니다");
+} 
+else if(true) {
+console.log("else if 1");
+} 
+else if(true){
+console.log("else if 2");
+} x
+else {
+console.log("else");
+} x
+
+
+
+
+// 한줄짜리 주석
+/*
+여러줄 짜리
+주석입니다
+*/
+/** 
+* 왠지 모르겠어
+*
+
+
+var greentea = "학자";
+
+if(greentea == "백마")
+{
+console.log( "참담한 마음");x
+}
+else if (greentea =="암기")
+{
+console.log("흑야");
+}
+else if( greetea == "용기사")
+console.log("천체강하");
+}
+else {
+console.log("전력 질주");
+}
+
+var 흑당 = "암흑기사";
+흑당 = "건브레이커";
+흑당 = "";
+true/ false 
+flase
+var nodata;
+
+if(흑당){
+console.log(흑당)
+}
+
+
+
+var greentea = 0;
+if (greentea) {
+  console.log("힐러 아님");
+} else {
+  console.log("false");
+}
+
+*
+*/
