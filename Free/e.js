85b9971c80168a72de50425fa2910bbafdc097d6
DESKTOP-KJH\User <zscf1212@naver.com>
2022-02-27 21:28:19 +0900

    2202272128

---
+++ Free/e.js
@@ -0,0 +1,28 @@
+console.log("hello world!");
+console.log("Black Sugar\n sleep \t wake up")
+console.log("4")
+console.log('"weird"') 
+console.log("\"of\"")
+
+console.log("\n      ♪      ○ \n         /l__l__/l    ♪ \n       /         l      \"Hello World!\"\n      ( ﾐ⌒ ●⌒ ﾐ  )  /   ★ Ku Po ★")
+
+
+
+var darknight = "블랙슈가" ;
+var one = "흑당" - "당"
+var two = "12" - "1"
+console.log(darknight);
+console.log(one);
+console.log(two);
+
+
+
+
+var pom ="       ♪      ○ "
+var ear ="\n         /l__l__/l    ♪ "
+var head ='\n        /         l      Hello World!'
+var smile ="\n       ( ﾐ⌒ ●⌒ ﾐ  )  /   ★ Ku Po ★"
+console.log(pom+ear+head+smile);
+
+
+
