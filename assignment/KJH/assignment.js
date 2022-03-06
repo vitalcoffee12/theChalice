4255ca583227152b62be81e7c93462a4493bb9cf
DESKTOP-KJH\User <zscf1212@naver.com>
2022-03-06 21:20:40 +0900

    숙제 수정

--- assignment/KJH/assignment.js
+++ assignment/KJH/assignment.js
@@ -1,17 +1,19 @@
 //탱커의 무적기 특징에 대하여 220227
 
-var invincibletime = 10;
-var getdamage = "X";
+var invincibletime = 101;
+var getdamage = "x";
 
-if(invincibletime==10 && getdamage=="X"){
+if(invincibletime==10 && getdamage=="x"){
   console.log("천하무적");
 }
-else if(invincibletime==8 && getdamage=="X"){
-  console.log("폭발유성");
-}
-else if(invincibletime==10 && getdamage!="X"){
+else if(invincibletime==10 && getdamage!="x"){
   console.log("산송장");
 }
-else {
+else if(invincibletime==8 && getdamage=="x"){
+  console.log("폭발유성");
+}
+else if(invincibletime==8 && getdamage!="x"){
   console.log("일대일대결");
 }
+else {
+  console.log("지속시간이랑 데미지 여부를 다시 확인해 주세요!");
