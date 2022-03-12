ba5e94f1c63622408cfb9ab93723fecacf7c2f1c
BlueLetter17 <skdd23@naver.com>
2022-03-12 17:18:02 +0900

    switch ,case 숙제 업로드

---
+++ Blue_Letter/switch.js
@@ -0,0 +1,15 @@
+var job = "pld";
+
+switch(job)
+{
+    case "sam":
+        console.log("사무라이");
+        break;
+
+    case "pld":
+        console.log("나이트");
+        break;
+    default :
+        console.log("디폴트");
+        break;
+}
