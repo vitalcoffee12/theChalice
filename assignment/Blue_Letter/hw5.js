604153f59eb534dea447a386709f88f23794865e
BlueLetter17 <skdd23@naver.com>
2022-03-27 00:13:51 +0900

    숙제 수정

--- assignment/Blue_Letter/hw5.js
+++ assignment/Blue_Letter/hw5.js
@@ -1,10 +1,12 @@
 var fcMembers =["녹차","흑당","렌쟈","레터","릴라"];
+console.log("[홀수]")
 for(var i=0; i<fcMembers.length; i++)
 {
     if(i%2==0){
         console.log(fcMembers[i]);
     }
 }
+console.log("[짝수]")
 for(i=0; i<fcMembers.length; i++)
 {
     if(i%2==1){
