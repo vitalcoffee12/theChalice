ad1ad8be2afb876250c2ee39a6b963db29e405cf
vc12 <vitalcoffee12@gmail.com>
2022-03-27 00:05:49 +0900

    [0327] while, 배열

--- assignment/KJH/220312.js
+++ assignment/KJH/220312.js
@@ -1,7 +1,10 @@
 // 숙제 1단부터 9단까지 구구단 만들기
 
-for (var GG = 1; GG <= 9; GG++)
-for (var TT = 1; TT <= 9; TT++){
-    console.log( GG, "*", 1*TT, "=", GG*TT)
-    if(1*TT == 9){console.log("-", GG, "단 쿠뽀-")}
+for (var GG = 1; GG <= 9; GG++) {
+  for (var TT = 1; TT <= 9; TT++) {
+    if (TT == 1) {
+      console.log("-", GG, "단 쿠뽀-");
+    }
+    console.log(GG, "*", 1 * TT, "=", GG * TT);
+  }
 }
