5118f1f9f7a0012bff908cf4109f8f298c8302b0
DESKTOP-O8ARQMO\송준우 <junwoosara@naver.com>
2022-04-24 21:14:20 +0900

    2022-04-24  게임만들기 기본틀

---
+++ rillasong/practice/javascript/prac20220330.js
@@ -0,0 +1,34 @@
+// var player={
+//     name:"릴라송",
+//     level:80,
+//     job:"사무라이",
+//     hp:1000,
+//     mp:1000
+    
+// };
+
+// var players=[];
+// console.log(player);
+
+// for(var i=0; i<100; i++){
+//     players.push(player);
+// }
+
+// console.log(players);
+
+// sum(1,2);
+
+
+
+function sum(num1,num2){
+    console.log(num1+num2);
+}
+function sub(num1,num2){
+    console.log(num1-num2);
+}
+function mux(num1,num2){
+    console.log(num1*num2);
+}
+function div(num1,num2){
+    console.log(num1%num2);
+}
